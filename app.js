ff15de9da459463308d14a8259f87be6d4f71f66
Robert Allison <rsallison1979@gmail.com>
2018-09-30 08:59:27 -0400

    Added Bootstrap Grid and customer styling to show page

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ seedDB();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("libs"));
+app.use(express.static(__dirname + "/public"));
 // ROUTES //
 app.get("/", function(req, res){
     res.render("landing");
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest",
         image: "https://cdn.pixabay.com/photo/2017/07/17/16/04/nature-2512898__340.jpg",
-        description: "A nice play with lots of clouds"
+        description: "Morbi id ex gravida, suscipit quam quis, mattis lacus. Maecenas sit amet tellus ac dolor rutrum malesuada nec eget nunc. Mauris sed libero fringilla massa cursus placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus facilisis, lobortis quam vitae, porta felis. Mauris ut rhoncus mauris. Donec placerat magna sed placerat dignissim. Donec nec dolor ullamcorper nisi dignissim sodales. Pellentesque sed pulvinar leo, at sodales nulla. Etiam at est ut odio posuere vehicula. Morbi quis sapien ac elit luctus tempus efficitur in tortor. Praesent interdum leo felis, non tempus mi euismod in. Duis nec libero sollicitudin leo lacinia sagittis. Sed blandit turpis sit amet est sollicitudin consequat. Duis tincidunt, purus in consequat sagittis, purus nulla gravida justo, finibus dictum orci neque eget magna. Curabitur nec elit sed erat fermentum commodo."
     },
     {
         name: "Rocky Hill",
         image: "https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197__340.jpg",
-        description: "A nice play with a rocky hill"
+        description: "Morbi id ex gravida, suscipit quam quis, mattis lacus. Maecenas sit amet tellus ac dolor rutrum malesuada nec eget nunc. Mauris sed libero fringilla massa cursus placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus facilisis, lobortis quam vitae, porta felis. Mauris ut rhoncus mauris. Donec placerat magna sed placerat dignissim. Donec nec dolor ullamcorper nisi dignissim sodales. Pellentesque sed pulvinar leo, at sodales nulla. Etiam at est ut odio posuere vehicula. Morbi quis sapien ac elit luctus tempus efficitur in tortor. Praesent interdum leo felis, non tempus mi euismod in. Duis nec libero sollicitudin leo lacinia sagittis. Sed blandit turpis sit amet est sollicitudin consequat. Duis tincidunt, purus in consequat sagittis, purus nulla gravida justo, finibus dictum orci neque eget magna. Curabitur nec elit sed erat fermentum commodo."
     },
     {
         name: "Glacier Bay",
         image: "https://cdn.pixabay.com/photo/2018/05/16/15/49/camper-3406137__340.jpg",
-        description: "A nice play with a Glacier"
+        description: "Morbi id ex gravida, suscipit quam quis, mattis lacus. Maecenas sit amet tellus ac dolor rutrum malesuada nec eget nunc. Mauris sed libero fringilla massa cursus placerat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et metus facilisis, lobortis quam vitae, porta felis. Mauris ut rhoncus mauris. Donec placerat magna sed placerat dignissim. Donec nec dolor ullamcorper nisi dignissim sodales. Pellentesque sed pulvinar leo, at sodales nulla. Etiam at est ut odio posuere vehicula. Morbi quis sapien ac elit luctus tempus efficitur in tortor. Praesent interdum leo felis, non tempus mi euismod in. Duis nec libero sollicitudin leo lacinia sagittis. Sed blandit turpis sit amet est sollicitudin consequat. Duis tincidunt, purus in consequat sagittis, purus nulla gravida justo, finibus dictum orci neque eget magna. Curabitur nec elit sed erat fermentum commodo."
     }
 ];
 

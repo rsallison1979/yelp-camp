d89fdfbba8c4b43ee8ea5fedd24b3bf9d0045340
Robert Allison <rsallison1979@gmail.com>
2018-10-20 20:11:54 -0400

    Fix spelling error on flash message

--- middleware/index.js
+++ middleware/index.js
@@ -47,7 +47,7 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-    req.flash("error", "You need to be logged n to do that");
+    req.flash("error", "You need to be logged in to do that");
     res.redirect("/login");
 };
 
